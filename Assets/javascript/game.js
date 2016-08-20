7257cbb6c859364eeba548763daa4beba11bc795
Kevin Zhu <kzoo223@gmail.com>
2016-08-19 21:41:24 -0400

    initial commit

---
+++ Assets/javascript/game.js
@@ -0,0 +1,40 @@
+//characters
+var faith = {
+    hp: 120,
+    atk: 6,
+    counter: 10,
+}
+
+var pyro = {
+	hp: 100,
+	atk: 8
+	counter: 5
+}
+
+var quality {
+	hp: 120
+	atk: 5
+	counter: 25
+}
+
+var int = {
+	hp: 115 
+	atk: 10
+	counter: 8
+}
+
+
+
+
+$('.up').append("<img src = 'assets/images/logo.jpg' class = 'img-responsive'>")
+
+
+//create character boxes
+
+//select character
+
+//move others to enemies
+
+//select enemy
+
+//attack button
