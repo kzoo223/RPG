7810018a7e4efa132b26f5e2f789fc7cb1498c95
Kevin Zhu <kzoo223@gmail.com>
2016-08-22 20:57:35 -0400

    add attack button on enemy selection

--- Assets/javascript/game.js
+++ Assets/javascript/game.js
@@ -4,19 +4,19 @@ $(document).ready(function(){
 //characters
 var classes = {
 
-	priest: {
+	priest:{
     	hp: 120,
     	atk: 6,
     	counter: 10
 	},
 
-	pyro: {
+	pyro:{
 		hp: 100,
 		atk: 8,
 		counter: 5
 	},
 
-	knight: {
+	knight:{
 		hp: 120,
 		atk: 5,
 		counter: 25
@@ -33,10 +33,10 @@ var classes = {
 
 
 //create character boxes
-	$("#priestClass").append('<img class = "character" data-selection = "false" data-character = "priest" src = "assets/images/priest.jpg">');
-	$("#knightClass").append('<img class = "character" data-selection = "false" data-character = "knght" src = "assets/images/knight.jpg">');
-	$("#sorcClass").append('<img class = "character" data-selection = "false" data-character = "sorc" src = "assets/images/sorc.jpg">');
-	$("#pyroClass").append('<img class = "character" data-selection = "false" data-character = "pyro" src = "assets/images/pyro.jpg">');
+	$("#priestClass").append('<img class = "character" data-selection="false" data-character="priest" src = "assets/images/priest.jpg">');
+	$("#knightClass").append('<img class = "character" data-selection="false" data-character="knght" src = "assets/images/knight.jpg">');
+	$("#sorcClass").append('<img class = "character" data-selection="false" data-character="sorc" src = "assets/images/sorc.jpg">');
+	$("#pyroClass").append('<img class = "character" data-selection="false" data-character="pyro" src = "assets/images/pyro.jpg">');
 
 
 //select character & select enemy	
@@ -45,31 +45,36 @@ $(".character").on("click", function (){
 	var selection = $(this).data("selection");
 	selection = true;
 
+		//character selection
 		if (selection && yourChoice === 0){
+			//appends your selection to your char div + displays your hp
 			$("#yourChar").append($(this));
 			yourChoice++
 			var friendly = $(this).data('character');
 			var attributes = classes[friendly]
 			$("#yourHealth").append(attributes.hp)
 		}
+
+		//enemy selection
 		else if(selection && yourChoice === 1){
+			//appends selection to enemy div + displays enemy hp
 			$("#enemySelected").append($(this));
 			yourChoice++;
 			$(document).removeClass('character');
 			var enemy = $(this).data('character');
 			var attributes = classes[enemy];
-			console.log(attributes);
+			$("#enemyHealth").append(attributes.hp)
+			//add attack button after selecting enemy
+			var atk = $('<button type="button" class="btn btn-secondary>');
+			atk.text("Attack");
+			$("#action").html(atk);
+
 		}
-
-
 });
 
 
 
 
-//move others to enemies
-
-//select enemy
 
 //attack button
 
