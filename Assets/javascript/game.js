3b07ecd44a653e56dd71272acfaed5ba1cc0242b
Kevin Zhu <kzoo223@gmail.com>
2016-08-22 22:14:52 -0400

    attack button functions

--- Assets/javascript/game.js
+++ Assets/javascript/game.js
@@ -34,13 +34,19 @@ var classes = {
 
 //create character boxes
 	$("#priestClass").append('<img class = "character" data-selection="false" data-character="priest" src = "assets/images/priest.jpg">');
-	$("#knightClass").append('<img class = "character" data-selection="false" data-character="knght" src = "assets/images/knight.jpg">');
+	$("#knightClass").append('<img class = "character" data-selection="false" data-character="knight" src = "assets/images/knight.jpg">');
 	$("#sorcClass").append('<img class = "character" data-selection="false" data-character="sorc" src = "assets/images/sorc.jpg">');
 	$("#pyroClass").append('<img class = "character" data-selection="false" data-character="pyro" src = "assets/images/pyro.jpg">');
 
 
 //select character & select enemy	
 var yourChoice = 0;
+var yourHP
+var enemyHP
+var yourAttack
+var counterAttack
+var attackButton
+//on character click
 $(".character").on("click", function (){
 	var selection = $(this).data("selection");
 	selection = true;
@@ -52,7 +58,8 @@ $(".character").on("click", function (){
 			yourChoice++
 			var friendly = $(this).data('character');
 			var attributes = classes[friendly]
-			$("#yourHealth").append(attributes.hp)
+			yourHP = $("#yourHealth").append("Your HP: " + attributes.hp)
+			yourAttack = attributes.atk
 		}
 
 		//enemy selection
@@ -63,19 +70,26 @@ $(".character").on("click", function (){
 			$(document).removeClass('character');
 			var enemy = $(this).data('character');
 			var attributes = classes[enemy];
-			$("#enemyHealth").append(attributes.hp)
+			enemyHP = $("#enemyHealth").append("Enemy HP: " + attributes.hp)
 			//add attack button after selecting enemy
-			var atk = $('<button type="button" class="btn btn-secondary>');
+			var atk = $('<button type="button" class="btn btn-secondary" id = "attack">');
 			atk.text("Attack");
 			$("#action").html(atk);
+			attackButton=atk
+			counterAttack=attributes.atk
+		}
+
+	//attack button
+		else if (selection && yourChoice === 2){
+			$("#attack").on("click", function(){
+				yourHP = yourHP - CounterAttack
+				
+			});
+
 
 		}
-});
-
-
-
-
-
-//attack button
-
+					
+	
+		
+	});
 });
